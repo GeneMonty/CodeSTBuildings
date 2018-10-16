e790058afed686d5b235b9dad22273ce16e008f1
GeneMonty <36083539+GeneMonty@users.noreply.github.com>
2018-10-16 03:55:56 -0500

    Update ReadMe.lua

--- ReadMe.lua
+++ ReadMe.lua
@@ -1,6 +1,5 @@
---[[
 
-The following MOD "SpectraBuildings" is property of bullettMAGNETT AKA GENEMONTY
+The following MOD "Spectra Buildings" is property of bullettMAGNETT AKA GENEMONTY
 
 Mesh and textures are original creations of myself, you have no right to copy or submit them as your own. Please respect my time creating these assets, for the community to enjoy them.
 
@@ -16,5 +15,3 @@ https://twitter.com/bullettmagnett
 Thanks for CheTranqui for Helping me BIG TIME to sort of an important part of the code! Thanks Dude!
 
 Also this mods would not be possible without the ModTools provided by HAEMIMONT GAMES. Support the Devs!
-
-]]
